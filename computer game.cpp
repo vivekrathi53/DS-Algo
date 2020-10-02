a153337986a2e252bcc6d06774ee726fca4842f1
Vivek Rathi <vivekrathi53@gmail.com>
2020-10-02 12:56:29 +0530

    Add files via upload

---
+++ computer game.cpp
@@ -0,0 +1,73 @@
+/*
+  Vivek Rathi
+  CSE 2nd Year
+  MNNIT
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+#define ll long long int
+#define pb push_back
+#define pf push_front
+#define pob pop_back
+#define pof pop_front
+#define debug1(x) cout<<#x<<" "<<x<<endl;
+#define debug2(x,y) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<endl;
+#define debug3(x,y,z) cout<<#x<<" "<<x<<" "<<#y<<" "<<y<<" "<<#z<<" "<<z<<endl;
+#define present(c,x) ((c).find(x) != (c).end())
+#define null NULL
+#define mp make_pair
+#define fi first
+#define se second
+#define boost ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define inf 1e18
+#define flush fflush(stdout);
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+#define ordered_set tree<ll, null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+ll modpower(ll a,ll b,ll c)
+{
+	ll res=1;
+	while(b)
+	{
+		if(b&1LL)
+			res=(res*a)%c;
+		a=(a*a)%c;
+		b>>=1;
+	}
+	return res;
+}
+//-------------------------------Template--Above------------------------------------------------
+
+
+int main()
+{
+	boost
+	ll t;
+	cin>>t;
+	while(t--)
+	{
+		ll k,n,a,b;
+		cin>>k>>n>>a>>b;
+		ll low=0,high=n,ans=-1;
+		while(low<=high)
+		{
+			ll mid=(low+high)/2;
+			ll temp=k-a*mid;
+			temp-=b*(n-mid);
+			if(temp+b<=b)
+			{
+				high=mid-1;
+			}
+			else
+			{
+				ans=mid;
+				low=mid+1;
+			}
+		}
+		cout<<ans<<endl;
+	}
+
+	return 0;
+}
